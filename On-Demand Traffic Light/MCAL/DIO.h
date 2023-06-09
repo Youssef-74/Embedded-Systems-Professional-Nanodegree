0f1a49c5fb156a4c70e71743ce4c4379400cbd61
Youssef Essam <yosifesam98@gmail.com>
2023-06-09 15:01:09 +0300

    Add files via upload

---
+++ On-Demand Traffic Light/MCAL/DIO.h
@@ -0,0 +1,46 @@
+/*
+ * DIO.h
+ *
+ * Created: 9/10/2022 8:55:39 PM
+ *  Author: Hossam
+ */ 
+
+#ifndef DIO_H_
+#define DIO_H_
+#include "../../Utilities/types.h"
+#include "../../Utilities/Registers.h"
+#include "../Timer driver/timer.h"
+#include "../../Utilities//interrupts.h"
+
+// all internal typedefs
+typedef enum EN_PortDirection_t 
+{
+	INPUT , OUTPUT
+}EN_PortDirection_t;
+
+typedef enum EN_PortValue_t 
+{
+    LOW , HIGH
+}EN_PortValue_t;
+
+// all driver macros 
+
+#define SET_BIT(X,BIT_NO) X |= (1<<BIT_NO)
+#define CLEAR_BIT(X,BIT_NO) X &= ~(1<<BIT_NO)
+#define TOGGLE_BIT(X,BIT_NO) X ^= (1<<BIT_NO)
+#define READ_BIT(X,BIT_NO) (X&(1<<BIT_NO))>>BIT_NO
+#define PORT_A  'A'
+#define PORT_B  'B'
+#define PORT_C  'C'
+#define PORT_D  'D'
+
+// DIO APIs
+ 
+ void DIO_init(uint8_t PortNumber , uint8_t PinNumber , uint8_t direction ); // Initialize DIO direction 
+ void DIO_write(uint8_t PortNumber , uint8_t PinNumber , uint8_t value); // write data to DIO 
+ void DIO_toggle(uint8_t PortNumber , uint8_t PinNumber); // toggle DIO 
+ void DIO_read(uint8_t PortNumber , uint8_t PinNumber, uint8_t *value); // read DIO 
+
+
+
+#endif /* DIO_H_ */
---
+++ On-Demand Traffic Light/MCAL/timer.h
@@ -0,0 +1,38 @@
+/*
+ * timer.h
+ *
+ * Created: 9/11/2022 8:43:48 PM
+ *  Author: Hossam
+ */ 
+
+#include "../DIO driver/DIO.h"
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+// USER defined macros 
+#define NORMAL 'N'
+#define CTC    'C'
+#define FAST_PWM 'F'
+#define PWM_PC 'P'
+#define NO_PRESCALAR '0'
+#define PRESCALAR_8 '1'
+#define PRESCALAR_64 '2'
+#define PRESCALAR_256 '3'
+#define PRESCALAR_1024 '4'
+
+
+// Timer APIs
+void Timer_init( uint8_t MODE , uint8_t Initial_Value); //Initialize the timer by defining the mode and the initial value
+void Timer_Start(unsigned int PRESCALAR); // Start the timer by defining the prescalar 
+void Timer_Stop(); // Stop the timer 
+void Clear_OVERFLOWFLAG(); // Clear overflow flag by setting bit 0 in TIFR register 
+void Timer_5sec(); // Start a timer with 5 seconds delay  
+void Timer_1sec(); // start a timer with 1 second delay 
+void Timer_halfsec();
+uint8_t Timer_status(); // Get the timer status by checking on the overflow flag 
+
+
+
+
+#endif /* TIMER_H_ */
